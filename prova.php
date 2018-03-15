9b7b9ccbcbcc6a52d0c44539a932240483cab27e
misterfox81 <lspezia81@gmail.com>
2018-03-15 14:03:43 +0100

    Modifica al file principale

--- prova.php
+++ prova.php
@@ -6,7 +6,7 @@
     <title>Prova bash</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
+    <script src="js/main.js"></script>
 </head>
 <body>
 <?
